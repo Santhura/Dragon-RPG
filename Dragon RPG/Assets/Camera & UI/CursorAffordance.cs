16827154c89d59d0a010e0e0a5fa30f1890d3fa5
Dylan Gijsbertsen PC <d.b.gijsbertsen@live.nl>
2017-10-19 17:05:00 +0200

    Organize folders, added new cursor for each layer, added debug mode for walking WASD and cursor

---
+++ Dragon RPG/Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] private Texture2D walkCursor = null;
+    [SerializeField] private Texture2D attackCursor = null;
+    [SerializeField] private Texture2D unknownCursor = null;
+
+    [SerializeField] private Vector2 cursorHotspot = new Vector2(96, 96);
+
+    CameraRaycaster camRayCaster;
+
+	// Use this for initialization
+	void Start () {
+        camRayCaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        switch (camRayCaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(attackCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            default:
+                Debug.LogError("don't know what cursor to show");
+                return;
+        }
+	}
+}
